28a4ed3e28f2088276bb4010c69b0532671ee609
acasasaez <acasasae@myuax.com>
2022-05-07 09:26:17 +0200

    lectura de los archivos del csv

---
+++ lectura_limpieza_csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df_conversiones= pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/fresas_con_yogur/conversiones (4).csv", sep = ";")
+print (df_conversiones)
+
+df_navegacion = pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/fresas_con_yogur/navegacion (4).csv", sep = ";")
+print(df_navegacion)
