76e4084ae173aadc1836ed61917ffc1e6f68ab92
acasasaez <acasasae@myuax.com>
2022-05-07 11:00:36 +0200

    limpieza csv

--- lectura_limpieza_csv.py
+++ lectura_limpieza_csv.py
@@ -1,24 +1,17 @@
 #1) Comenzamos importando pandas para poder leer nuestros archivos csv
 #2) Nos damos cuenta de que en nuestros archivos csv los elementos de las filas vienen separados por puntos y cmas;
 #por lo tanto es necesario indicar el separador (sep =";") para que sea posible la lectura de nuestro archivo csv.
+from urllib.parse import urlparse
 import pandas as pd
 df_conversiones= pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/fresas_con_yogur/conversiones (4).csv", sep = ";")
-print (df_conversiones)
+
 
 df_navegacion = pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/fresas_con_yogur/navegacion (4).csv", sep = ";")
-print(df_navegacion)
 
-#A continuación procederemos a extraer los datos necesarios de nuestras URL (camp:campaña; adg:adgroup; adv:advertisement; sl:sitelink ), 
-#para ellos realizaremos las funciones que nos devuelvan dichos resultados.
-def obtener_campaña():
-    #sin hacer 
 
-def obtener_adg():
-    #sin hacer 
+#a continuación limpairemos el dataset, para esto emplearemos el método dropna de pandas, que nos permitirá eliminar aquellas filas que cuenten con algún valor nulo
+df_conversiones=df_conversiones.dropna()
+df_navegacion = df_navegacion.dropna()
 
-def obtener_adv():
-    #sin hacer 
-
-def obtener_sl():
-    #sin hacer 
-
+#print (df_conversiones)
+#print(df_navegacion)
