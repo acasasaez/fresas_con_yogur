b80282716f4ca8833ee4ad6d2f6d859213a690ad
acasasaez <acasasae@myuax.com>
2022-05-09 18:15:53 +0200

    :)

--- lectura_limpieza_csv.py
+++ lectura_limpieza_csv.py
@@ -15,19 +15,23 @@ df_navegacion = pd.read_csv(r"C:/Users/andre/OneDrive/Escritorio/Programación/f
 df_conversiones=df_conversiones.dropna()
 df_navegacion = df_navegacion.dropna()
 
-print (df_conversiones)
-print(df_navegacion)
+#print (df_conversiones)
+#print(df_navegacion)
 
 #Pasamos a la fase 1.2, ahora debemos obtener la siguiente información a partir de nuestra URL (camp: campaña; adg:adgroup; adv:advertisement; sl:sielink)
-#Para eso comenzamos importando urllib 
+
 
 #a continuación elaboraremos las funciones que nos permitan obtener dicha información a partir de las url 
-def obtener_camp(): 
-    campos =[]
-    for i in df_navegacion["url_landing"]:
-        url = i 
-        url = url.split("&")
-        campos.append(url[3])
-    return campos
-campos = obtener_camp()
-print(campos)
+#En la primera función necesitamos extraer todas las url de nuestro dataset
+def extraerURL (DataFramen_column =[]): #Nuestra función actuará sobre la columna del dataframe introducida 
+    i=int(input("Itroduzca un número entero: ")) #damos un valor entero a i
+
+    if i < len(DataFramen_column): #mientras i esté en el rango de la lista
+        Url = DataFramen_column[i]
+        return Url #nuestra función nos devuelve el elemento de la columna del dataframe en esta posición 
+    else:
+        print("Nuestro dataset no cuenta con elementos en dicha posición, por favor introduzca otro valor") #en caso contrario se da un mensaje que nos indica que estamos buscando una posición que no existe en nuestra lista 
+        return extraerURL(DataFramen_column) #la función se repite hasta introducir un valor válido 
+
+#extraccion_URL =extraerURL(df_navegacion["url_landing"])
+#print(extraccion_URL)
